8e2c941b0830482ef4f0224d8da16fb416771fb6
tristan5293 <tristan5293@gmail.com>
2017-07-27 16:58:36 +0800

    將Americas轉換為America，這樣才不會讓date_default_timezone_set()無法辨識

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,14 @@ Route::get('/get_server_time', function () {
         //throw new ProcessFailedException($process2);
         return $process->getErrorOutput();
     }else{
-        date_default_timezone_set(trim($process->getOutput()));
-        return date("Y-m-d h:i:s");
+        $origin_timezone = trim($process->getOutput());
+        if(str_contains($origin_timezone, 'Americas')){
+            $new_timezone = str_replace('Americas', 'America', $origin_timezone);
+            date_default_timezone_set($new_timezone);
+            return date("Y-m-d h:i:s");
+        }else{
+            date_default_timezone_set($origin_timezone);
+            return date("Y-m-d h:i:s");
+        }
     }
 });
