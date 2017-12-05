04e92b2d7cf44dd9db53dbd18a044ba8776ed2ae
tristan5293 <tristan5293@gmail.com>
2017-12-05 13:50:50 +0800

    網路設定資料寫入檔案(CentOS)

--- app/Http/Controllers/NetworkConfig.php
+++ app/Http/Controllers/NetworkConfig.php
@@ -115,43 +115,91 @@ class NetworkConfig extends Controller
         $master_dns = $request->input('master_dns');
         $slave_dns = $request->input('slave_dns');
 
-        $file_path = '/etc/network/interfaces';
-        $contents = Storage::get($file_path);
-        if(str_contains($contents, 'The primary network interface')){
+        if(env('APP_OS') == "ubuntu"){
+            $file_path = '/etc/network/interfaces';
+            $contents = Storage::get($file_path);
+            if(str_contains($contents, 'The primary network interface')){
+                $index = -1;
+                $data_arr = explode("\n", $contents);
+                $primary_network_key = false;
+                foreach ($data_arr as &$value) {
+                    $index++;
+                    //先找到標題在取得內容
+                    if(str_contains($value, 'The primary network interface')){
+                        $primary_network_key = true;
+                        continue;
+                    }
+                    if($primary_network_key){
+                        if(str_contains($value, 'address')){
+                            $data_arr[$index] = "address ".$ip;
+                            continue;
+                        }
+                        if(str_contains($value, 'netmask')){
+                            $data_arr[$index] = "netmask ".$netmask;
+                            continue;
+                        }
+                        if(str_contains($value, 'gateway')){
+                            $data_arr[$index] = "gateway ".$gateway;
+                            continue;
+                        }
+                        if(str_contains($value, 'dns-nameservers')){
+                            $data_arr[$index] = trim("dns-nameservers ".$master_dns." ".$slave_dns);
+                            break;
+                        }
+                    }
+                }
+                Storage::put($file_path, implode("\n", $data_arr));
+                //$contents = Storage::get($file_path);
+
+                //restart network
+                //$process = new Process('sudo ip addr flush ens33 && systemctl restart networking.service');
+                $process = new Process('sudo reboot');
+                $process->run();
+                // executes after the command finishes
+                if (!$process->isSuccessful()) {
+                    throw new ProcessFailedException($process);
+                }
+                return str_replace("\n","<br/>", $process->getOutput());
+            }else{
+                //第一次設定網路
+                Storage::append($file_path, "\n# The primary network interface");
+                Storage::append($file_path, "auto eth0");
+                Storage::append($file_path, "iface eth0 inet static");
+                Storage::append($file_path, "address ".$ip);
+                Storage::append($file_path, "netmask ".$netmask);
+                Storage::append($file_path, "gateway ".$gateway);
+                Storage::append($file_path, trim("dns-nameservers ".$master_dns." ".$slave_dns)."\n");
+                return '設定完成';
+            }
+        }else{
+            $file_path = '/etc/sysconfig/network-scripts/ifcfg-eth0';
+            $contents = Storage::get($file_path);
             $index = -1;
             $data_arr = explode("\n", $contents);
-            $primary_network_key = false;
             foreach ($data_arr as &$value) {
                 $index++;
-                //先找到標題在取得內容
-                if(str_contains($value, 'The primary network interface')){
-                    $primary_network_key = true;
+                if(str_contains($value, 'IPADDR0')){
+                    $data_arr[$index] = "IPADDR0=".$ip;
                     continue;
                 }
-                if($primary_network_key){
-                    if(str_contains($value, 'address')){
-                        $data_arr[$index] = "address ".$ip;
-                        continue;
-                    }
-                    if(str_contains($value, 'netmask')){
-                        $data_arr[$index] = "netmask ".$netmask;
-                        continue;
-                    }
-                    if(str_contains($value, 'gateway')){
-                        $data_arr[$index] = "gateway ".$gateway;
-                        continue;
-                    }
-                    if(str_contains($value, 'dns-nameservers')){
-                        $data_arr[$index] = trim("dns-nameservers ".$master_dns." ".$slave_dns);
-                        break;
-                    }
+                if(str_contains($value, 'NETMASK0')){
+                    $data_arr[$index] = "NETMASK0=".$netmask;
+                    continue;
+                }
+                if(str_contains($value, 'GATEWAY0')){
+                    $data_arr[$index] = "GATEWAY0=".$gateway;
+                    continue;
+                }
+                if(str_contains($value, 'DNS1')){
+                    $data_arr[$index] = "DNS1=".$master_dns;
+                    continue;
+                }
+                if(str_contains($value, 'DNS2')){
+                    $data_arr[$index] = "DNS2=".$slave_dns;
+                    break;
                 }
             }
             Storage::put($file_path, implode("\n", $data_arr));
-            $contents = Storage::get($file_path);
-
-            //restart network
-            //$process = new Process('sudo ip addr flush ens33 && systemctl restart networking.service');
             $process = new Process('sudo reboot');
             $process->run();
             // executes after the command finishes
@@ -159,16 +207,6 @@ class NetworkConfig extends Controller
                 throw new ProcessFailedException($process);
             }
             return str_replace("\n","<br/>", $process->getOutput());
-        }else{
-            //第一次設定網路
-            Storage::append($file_path, "\n# The primary network interface");
-            Storage::append($file_path, "auto eth0");
-            Storage::append($file_path, "iface eth0 inet static");
-            Storage::append($file_path, "address ".$ip);
-            Storage::append($file_path, "netmask ".$netmask);
-            Storage::append($file_path, "gateway ".$gateway);
-            Storage::append($file_path, trim("dns-nameservers ".$master_dns." ".$slave_dns)."\n");
-            return '設定完成';
         }
     }
 }
