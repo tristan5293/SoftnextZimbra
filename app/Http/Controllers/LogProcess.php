38897bf04ee93981deb965dfa4a6cfaadc78b29f
tristan5293 <tristan5293@gmail.com>
2017-12-15 16:26:53 +0800

    syslog 加入時間範圍搜尋(ubuntu)

--- app/Http/Controllers/LogProcess.php
+++ app/Http/Controllers/LogProcess.php
@@ -164,17 +164,41 @@ class LogProcess extends Controller
             $index++;
         }
 
+        $from_arr = explode("-", $from);
+        $to_arr = explode("-", $to);
+        $dt = Carbon::create($from_arr[0], $from_arr[1], $from_arr[2], 0);
+        $dt2 = Carbon::create($to_arr[0], $to_arr[1], $to_arr[2], 0);
+        $search_date_range_arr = array();
+
         if($keyword_arr[0] != ''){
             $tmp = '';
             if(env('APP_OS') == "ubuntu"){
-                $process = new Process('sudo zgrep -ai "'.$keyword_arr[0].'" /var/log/syslog* '.$grep_str);
+                //判斷符合搜尋日期條件的檔案並抓取出來
+                $str_syslog = '';
+                $collen_date_match = array();
+                $process_date_match = new Process('sudo ls -l --full-time /var/log/syslog* | awk \'{print $6 "\t" $9}\'');
+                $process_date_match->setTimeout(600);
+                $process_date_match->start();
+                foreach ($process_date_match as $type => $data) {
+                    if ($process_date_match::OUT === $type) {
+                        $tmp_arr = array();
+                        $tmp_arr = explode("\t", $data);
+                        $match_date = explode("-", $tmp_arr[0]);
+                        $dt3 = Carbon::create($match_date[0], $match_date[1], $match_date[2], 0);
+                        if($dt->lte($dt3) && $dt3->lte($dt2)){
+                            $collen_date_match[] = $tmp_arr[1];
+                        }
+                    } else { // $process::ERR === $type
+                        //process error msg;
+                    }
+                }
+                foreach ($collen_date_match as &$value) {
+                    $str_syslog .= $value.' ';
+                }
+                $process = new Process('sudo zgrep -ai "'.$keyword_arr[0].'" '.$str_syslog.' '.$grep_str);
             }else{
+                //for centos
                 //計算要搜尋的範圍日期
-                $from_arr = explode("-", $from);
-                $to_arr = explode("-", $to);
-                $dt = Carbon::create($from_arr[0], $from_arr[1], $from_arr[2], 0);
-                $dt2 = Carbon::create($to_arr[0], $to_arr[1], $to_arr[2], 0);
-                $search_date_range_arr = array();
                 $str_messages = '';
                 if($dt->lt($dt2)){
                     while($dt->lte($dt2)){
