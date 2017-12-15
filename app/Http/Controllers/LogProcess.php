8421d8fa922b609c74cb130f919c36a7a4323fea
tristan5293 <tristan5293@gmail.com>
2017-12-15 11:35:57 +0800

    syslog 加入時間範圍搜尋(centos)

--- app/Http/Controllers/LogProcess.php
+++ app/Http/Controllers/LogProcess.php
@@ -148,6 +148,8 @@ class LogProcess extends Controller
     }
 
     public function SysLog(Request $request){
+        $from = $request->input('send_date_form');
+        $to = $request->input('send_date_to');
         $keyword = $request->input('keyword');
         $keyword_arr = explode(" ", $keyword);
         $grep_str = '';
@@ -167,7 +169,26 @@ class LogProcess extends Controller
             if(env('APP_OS') == "ubuntu"){
                 $process = new Process('sudo zgrep -ai "'.$keyword_arr[0].'" /var/log/syslog* '.$grep_str);
             }else{
-                $process = new Process('sudo zgrep -ai "'.$keyword_arr[0].'" /var/log/messages* '.$grep_str);
+                //計算要搜尋的範圍日期
+                $from_arr = explode("-", $from);
+                $to_arr = explode("-", $to);
+                $dt = Carbon::create($from_arr[0], $from_arr[1], $from_arr[2], 0);
+                $dt2 = Carbon::create($to_arr[0], $to_arr[1], $to_arr[2], 0);
+                $search_date_range_arr = array();
+                $str_messages = '';
+                while($dt->gte($dt2)){
+                    $search_date_range_arr[] = $dt->year.str_pad($dt->month,2,'0',STR_PAD_LEFT).str_pad($dt->day,2,'0',STR_PAD_LEFT);
+                    $dt->subDay();
+                }
+                if($dt->eq($dt2)){
+                    $str_messages = '/var/log/messages';
+                }else{
+                    foreach ($search_date_range_arr as &$value) {
+                        $str_messages .= '/var/log/messages-'.$value.'.gz ';
+                    }
+                    $str_messages .= '2>/dev/null'; //代表忽略掉错误提示信息。
+                }
+                $process = new Process('sudo zgrep -ai "'.$keyword_arr[0].'" '.$str_messages.' '.$grep_str);
             }
             $process->setTimeout(600); // 10 minutes
             $process->start();
--- resources/views/zimbra/sys_log.blade.php
+++ resources/views/zimbra/sys_log.blade.php
@@ -16,6 +16,40 @@
                 $('#result').textbox({
                     height: $(window).height() - 50,
                 });
+
+                $('#send_date_form').datebox().datebox('calendar').calendar({
+                    validator: function(date){
+                        var now = new Date();
+                        var d1 = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+                        return d1>=date;
+                    }
+                });
+                $('#send_date_to').datebox().datebox('calendar').calendar({
+                    validator: function(date){
+                        var now = new Date();
+                        var d1 = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+                        return d1>=date;
+                    }
+                });
+                $('#send_date_form').datebox({
+                    editable:false,
+                    showSeconds:false,
+                    //formatter 是在顯示時如何選擇日期後將其格式化為所需的格式.
+                    formatter:function(date){ return date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate(); },
+                    //parser 是在選擇好日期後告訴控件如何去解析自定義的格式.
+                    parser:function(date){ return new Date(Date.parse(date.replace(/-/g,"/"))); }
+                });
+                $('#send_date_to').datebox({
+                    editable:false,
+                    showSeconds:false,
+                    //formatter 是在顯示時如何選擇日期後將其格式化為所需的格式.
+                    formatter:function(date){ return date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate(); },
+                    //parser 是在選擇好日期後告訴控件如何去解析自定義的格式.
+                    parser:function(date){ return new Date(Date.parse(date.replace(/-/g,"/"))); }
+                });
+                var d = new Date();
+                $('#send_date_form').datebox('setValue', d.getFullYear()+'-'+(d.getMonth()+1)+'-'+d.getDate());
+                $('#send_date_to').datebox('setValue', d.getFullYear()+'-'+(d.getMonth()+1)+'-'+d.getDate());
 		        });
 
             function submitCheckSysLog(){
@@ -41,6 +75,11 @@
                 <td>
                     <input class="easyui-textbox" id="keyword" name="keyword"
                            style="width:300px;height:32px" data-options="prompt:'請輸入欲查詢之關鍵字'">
+                    搜尋範圍：
+                    從 <input id="send_date_form" name="send_date_form" class="easyui-datebox"
+                             style="width:120px;height:32px"></input>
+                    到 <input id="send_date_to" name="send_date_to" class="easyui-datebox"
+                             style="width:120px;height:32px"></input>
                     <a href="javascript:void(0)" class="easyui-linkbutton" id="btnSubmit"
                        style="width:75px;height:32px" onclick="submitCheckSysLog()">
                        送出
